a73543d7c4a94ef50c811a606ff7d87ef5257dd2
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-20 02:59:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,17 @@
-def unsafe?(speed)
-  #unsafe
-  if speed > 60
-  puts true
- end
-end
+# def unsafe?(speed)
+#   #unsafe
+#   if speed > 60
+#   puts true
+# end
+# end
 
 
 
-def not_safe?(speed)
+# def not_safe?(speed)
 	
+# end
+def not_safe?(speed)
+  speed < 40 || speed > 60
 end
-
 
 
